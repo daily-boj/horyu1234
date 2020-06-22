088b1bb6e5936fec4e47c1b6b9e5b9f9430a34af
Hyunoh Ryu <horyu1234@naver.com>
2020-06-22 17:06:25 +0900

    14581번 문제 소스 코드 추가

---
+++ src/P14581.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val name = sc.nextLine()
+    println(":fan::fan::fan:")
+    println(":fan::$name::fan:")
+    println(":fan::fan::fan:")
+}
